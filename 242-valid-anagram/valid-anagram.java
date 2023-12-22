08dca749df132ac139e2a823a7c65542ed79fbd2
Haripriya <arrahppk@gmail.com>
2023-12-22 15:32:00 +0530

    Time: 16 ms (20.05%) | Memory: 44.8 MB (6.44%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        if(s.length()!=t.length())
+           return false;
+        for(char c:s.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)+1);
+            }
+            else{
+                map.put(c,1);
+            }
+        }
+        for(char c:t.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)-1);
+            }
+        }
+        Set<Character> keys = map.keySet();
+
+        for(char key:keys){
+            if(map.get(key)!=0)
+                return false;
+        }
+        return true;
+    }
+}
